927b53ea097283802fa45f3231fc4bb9c98720e9
dwang888 <dwang888@outlook.com>
2018-07-16 01:38:46 -0400

    demo to show React index to call sub js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+
+// import VisTest from './vis_test';
+// import VisTest2 from './vis_test2';
+import VisScatter from './vis_scatter';
+import VisLine from './vis_line';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<VisScatter />, document.getElementById('root'));
+// registerServiceWorker();
